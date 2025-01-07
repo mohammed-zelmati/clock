f9526ea6db216d6ee78c4e1c5367d5201727bc8d
unknown <paul-emmanuel.buffe@laplateforme.io>
2025-01-07 21:55:42 +0100

    seconde option alarme

--- alarm2.py
+++ alarm2.py
@@ -1,13 +1,5 @@
 import time
 
-# extraction des éléments utiles depuis horloge locale
-local_time = time.localtime()
-tuple = (local_time.tm_hour, local_time.tm_min, local_time.tm_sec)
-
-def impression(new_tuple):
-      print(f"{new_tuple[0]:02} : {new_tuple[1]:02} : {new_tuple[2]:02}", end="\r", flush=True)
-
-
 def alarm():
     h_alarm = int(input("Veuillez renseigner l'heure :"))
     m_alarm = int(input("Veuillez renseigner les minutes :"))
@@ -21,7 +13,7 @@ def alarm():
             local_secondes = local_time.tm_sec
 
             if h_alarm == local_hour and m_alarm == local_minutes:
-                print("Alarm on")
+                print("Alarm on       ",end="\r")
 
             else:
                   print(f"{local_time.tm_hour:02} : {local_time.tm_min} : {local_time.tm_sec}",end="\r", flush=True)
